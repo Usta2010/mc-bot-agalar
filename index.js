c09126741db6d96922ba767ba1e27d3d37d39e68
Usta2010 <89610412+Usta2010@users.noreply.github.com>
2026-02-14 10:19:51 +0300

    Create a Minecraft bot with web server
    Implement a Minecraft bot using mineflayer and set up a web server to indicate bot status.

---
+++ index.js
@@ -0,0 +1,30 @@
+const mineflayer = require('mineflayer')
+const http = require('http');
+
+// Render'ın uyumaması için gereken web sunucusu
+http.createServer((req, res) => {
+    res.write("Bot Aktif!");
+    res.end();
+}).listen(10000); // Render genelde 10000 portunu sever
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'agalarmclise.falix.gg', 
+        port: 25565,
+        username: 'Bot_Agalar', 
+        version: '1.21',
+        hideErrors: true
+    })
+
+    bot.on('login', () => console.log('✅ Bot sunucuda!'));
+    bot.on('spawn', () => {
+        setInterval(() => {
+            if (bot.entity) {
+                bot.setControlState('jump', true);
+                setTimeout(() => bot.setControlState('jump', false), 500);
+            }
+        }, 15000);
+    });
+    bot.on('end', () => setTimeout(createBot, 10000));
+}
+createBot();
